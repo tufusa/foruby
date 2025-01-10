952aa881f545e8cf3d3ba7fb9a8812103afde652
tufusa <tufusa74@gmail.com>
2025-01-10 13:32:41 +0900

    chore: 用例を追加

--- src/target.rb
+++ src/target.rb
@@ -5,17 +5,16 @@ require './lib/foruby'
 using Foruby
 Foruby::Tracer.trace binding
 
-c = -1
-a = integer(10)
-b = integer(1).parameter
+c = 1
+a = integer.set
+b = integer.parameter.set(1)
 
 puts 'Hello, world!'
 puts ((1 + 1 * 4 - 2).abs + -8.abs / 2).abs / 3
 puts a
 puts b
 
-a = c
-a = b
+a = b + c
 
 puts a
 puts b

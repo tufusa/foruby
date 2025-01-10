d4412f9a4d6bb373e224cbc80eee3846e536b900
tufusa <tufusa74@gmail.com>
2025-01-10 10:56:19 +0900

    chore: 用例を更新

--- src/target.rb
+++ src/target.rb
@@ -1,8 +1,22 @@
 # frozen-string-literal: true
 
 require './lib/foruby'
+require './lib/tracer'
 
 using Foruby
+Foruby::Tracer.trace binding
+
+c = -1
+a = integer(10)
+b = integer(1).parameter
 
 puts 'Hello, world!'
 puts ((1 + 1 * 4 - 2).abs + -8.abs / 2).abs / 3
+puts a
+puts b
+
+a = c
+a = b
+
+puts a
+puts b

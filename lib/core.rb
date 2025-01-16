0fcbb7538d740a6589ca73be8c34799b2e2de63b
tufusa <tufusa74@gmail.com>
2025-01-16 11:59:18 +0900

    feat: fragmentsをBindingがキーのHashに変更 ブロック用の実装

--- lib/core.rb
+++ lib/core.rb
@@ -5,19 +5,44 @@ require_relative 'fragment'
 module Foruby
   # Foruby core module
   module Core
-    @@fragments = [Fragment.new(code: '')].clear # rubocop:disable Style/ClassVars
+    @@fragments = { binding => [Fragment.new(code: '')].clear }.clear # rubocop:disable Style/ClassVars
+    @@top_binding = binding # rubocop:disable Style/ClassVars
 
-    def self.fragments
-      @@fragments
+    def self.fragments = @@fragments
+
+    def self.top_binding = @@top_binding
+
+    def self.top_binding=(bin)
+      @@top_binding = bin # rubocop:disable Style/ClassVars
     end
 
     def self.check(code)
-      @@fragments.reject! { _1 == code } if code.is_a? Fragment
+      return unless code.is_a? Fragment
+
+      fragments.transform_values! { |fs| fs.reject { _1 == code } }
     end
 
-    def self.push(code)
+    def self.push(code, bin: nil)
+      key_binding = bin || top_binding
+      fragments[key_binding] = [] unless fragments.key? key_binding
+
       fragment = code.is_a?(Fragment) ? code : Fragment.new(code:)
-      fragment.tap { @@fragments << _1 }
+      fragment.tap { fragments[key_binding] << _1 }
+    end
+
+    def self.add_block(bin, &block)
+      # 一時的にtop_bindingを差し替える
+      previous_top_binding = top_binding
+      self.top_binding = bin
+
+      block.call
+
+      self.top_binding = previous_top_binding
+
+      body = Fragment.new code: fragments[bin].map(&:code).join("\n")
+      fragments.delete bin
+
+      body
     end
   end
 end

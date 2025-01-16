48434c85f88161f58de28bda4f0110c2be539a15
tufusa <tufusa74@gmail.com>
2025-01-16 12:00:21 +0900

    chore: 用例を追加

--- src/target.rb
+++ src/target.rb
@@ -3,11 +3,16 @@
 require './lib/foruby'
 
 using Foruby
+Foruby::Core.top_binding = binding
 Foruby::Tracer.trace binding
 
-c = 1
 a = integer.set
 b = integer.parameter.set(1)
+c = 1
+
+# TODO: ループ用変数を自動で宣言する
+i = integer.set
+sum = integer.set(0)
 
 puts 'Hello, world!'
 puts ((1 + 1 * 4 - 2).abs + -8.abs / 2).abs / 3
@@ -18,3 +23,10 @@ a = b + c
 
 puts a
 puts b
+
+(1..10).each do |i|
+  puts i
+  sum += i
+end
+
+puts sum

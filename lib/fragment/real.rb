9e08cbbfc4a6bdb8aa6d87b8968834de93c77dec
tufusa <tufusa74@gmail.com>
2025-01-24 10:45:48 +0900

    feat: realフラグメント

---
+++ lib/fragment/real.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require_relative '../fragment'
+
+module Foruby
+  # Fragment for real
+  class RealFragment < Fragment
+    def +(other)
+      Core.check self
+      Core.check other
+      self.class.new(code: "(#{inspect} + #{other.inspect})").tap { Core.push _1 }
+    end
+
+    def -(other)
+      Core.check self
+      Core.check other
+      self.class.new(code: "(#{inspect} - #{other.inspect})").tap { Core.push _1 }
+    end
+
+    def *(other)
+      Core.check self
+      Core.check other
+      self.class.new(code: "(#{inspect} * #{other.inspect})").tap { Core.push _1 }
+    end
+
+    def /(other)
+      Core.check self
+      Core.check other
+      self.class.new(code: "(#{inspect} / #{other.inspect})").tap { Core.push _1 }
+    end
+
+    def abs
+      Core.check self
+      self.class.new(code: "abs(#{inspect})").tap { Core.push _1 }
+    end
+
+    def ==(other)
+      Core.check self
+      Core.check other
+      LogicalFragment.new(code: "#{inspect} == #{other.inspect}").tap { Core.push _1 }
+    end
+
+    def !=(other)
+      Core.check self
+      Core.check other
+      LogicalFragment.new(code: "#{inspect} /= #{other.inspect}").tap { Core.push _1 }
+    end
+  end
+end

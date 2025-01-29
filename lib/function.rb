527d35a741435e4ddf2109ccae45391204de97b3
tufusa <tufusa74@gmail.com>
2025-01-29 17:03:33 +0900

    feat: 関数をインライン化できる

--- lib/function.rb
+++ lib/function.rb
@@ -6,9 +6,10 @@ module Foruby
     # @dynamic name, name=
     attr_accessor :name
 
-    def initialize(parameters, return_type, &block)
+    def initialize(parameters, return_type, inline: false, &block)
       @return_type = return_type
       @parameters = parameters
+      @is_inline = inline
       unless @parameters.size.equal? block.parameters.size
         raise ArgumentError,
               "Wrong number of block arguments (given #{block.parameters.size}, expected #{parameters.size})"
@@ -33,31 +34,38 @@ module Foruby
         raise ArgumentError,
               "Wrong number of arguments (given #{params.size}, expected #{@parameters.size})"
       end
+      if @is_inline
+        code = Fragment.new ''
+        Core.add_block(@function.binding) { code = @function[*params] }
+        return code
+      end
 
-      params.each { Core.check _1 }
-      Fragment.new code: "#{name}(#{params.join ','})"
+      Fragment.new "#{name}(#{params.join ','})"
     end
 
     # @dynamic []
     alias [] call
 
     def definition
+      return Fragment.new '' if @is_inline
+
       @definer[name]
     end
 
     private
 
     def definer
-      params = @parameters.map do |key, type|
-        fragment = case type
-                   when IntegerBuilder
-                     IntegerFragment.new(code: '')
-                   else
-                     Fragment.new(code: '')
-                   end
-        fragment.tap { _1.variable = Variable.new key, 0 }
+      params = @parameters.map do |name, type|
+        case type
+        when IntegerBuilder then IntegerFragment.new('').tap { _1.variable = Variable.new name, 0 }
+        when RealBuilder then RealFragment.new('').tap { _1.variable = Variable.new name, 0.0 }
+        when LogicalBuilder then LogicalFragment.new('').tap { _1.variable = Variable.new name, false }
+        else raise NotImplementedError, 'Not impletemted type'
+        end
       end
-      params_definition = @parameters.keys.map { "integer, intent(in) :: #{_1}" }.join "\n"
+      params_definition = @parameters.map do |name, type|
+        type.declaration name.to_s
+      end.join "\n"
       scope = Core.add_block @function.binding do
         @function[*params]
       end
@@ -68,31 +76,28 @@ module Foruby
       lambda do |name|
         body.gsub! hash.to_s, name.to_s
 
-        Fragment.new code: <<~CODE
-          integer function #{name}(#{@parameters.keys.join ','}) result(ret_#{name})
+        Fragment.new <<~CODE.chomp
+          function #{name}(#{@parameters.keys.join ','}) result(ret_#{name})
             #{uses}
-
             implicit none
 
             #{params_definition}
             #{variables}
+            #{@return_type&.declaration("ret_#{name}")}
 
             #{body}
           end function #{name}
         CODE
-          .chomp
       end
     end
 
     def result(res)
-      Core.check res
-      Core.push <<~CODE
+      Core.push <<~CODE.chomp
         ret_#{hash} = #{res}
         return
       CODE
-        .chomp
 
-      res.is_a?(Fragment) ? res : Fragment.new(code: res.inspect)
+      res.is_a?(Fragment) ? res : Fragment.new(res.inspect)
     end
   end
 end
